0165b8089ba0a4b14008d72dc326972bc79164bc
Alexander Aspman <aspmanalexander@gmail.com>
2020-02-17 13:40:39 +0100

    Update Skillz.js
    xcxc

--- src/components/Skillz.js
+++ src/components/Skillz.js
@@ -44,7 +44,7 @@ class Skillz extends React.Component  {
                   <div className="mt-5">
                             <div className="skillCard" > <img className="skillCard1" src={react} /> </div>     
                                       <h3 className="h4 mb-2">React</h3>
-                      <p className="text mb-0"style={{color:"#ffffff"}}>I have a good understanding of react but I am learning react-typescript there my knowleadg is a litle better then top of basic</p>
+                      <p className="text mb-0"style={{color:"#ffffff"}}>I have a good understanding of react, but I am learning react-typescript there my knowledge is just titled better most.</p>
                   </div>
               </div>
               <div className="col-lg-3 col-md-6 text-center">
